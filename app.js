0c7c80aa59d8b97a055df73611a729f409f6a632
Eric P. Katz <ericpkatz@gmail.com>
2017-05-09 15:09:52 -0400

    commit

--- app.js
+++ app.js
@@ -6,11 +6,22 @@ const app = express();
 
 module.exports = app;
 
+let config = process.env; 
+try {
+  config = require('./config.json');
+}
+catch(ex){
+  
+};
+console.log(config)
+
+app.set('view engine', 'ejs');
+
 app.use(require('body-parser').json());
 
 app.use('/vendor', express.static(path.join(__dirname, 'node_modules')));
 app.use('/dist', express.static(path.join(__dirname, 'dist')));
 
-app.get('/', (req, res, next)=> res.sendFile(path.join(__dirname, 'index.html')));
+app.get('/', (req, res, next)=> res.render('index', { GOOGLE_PLACES_KEY: config.GOOGLE_PLACES_KEY }));
 
 app.use('/api', require('./routes'));
